dd6362f00692d6fc9ddac1d10f82f82a294f2e5a
Duong Dai Phat <duongphat521@gmail.com>
2024-08-31 21:00:27 +0700

    add main.cpp

---
+++ BTLOOP/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World";
+	return 0;
+}
+
+//head file//
+// thuvien.h
+// void HelloWorld();
+
+//RF
+
+// void HelloWorld(){ cout << HelloWorld;}
